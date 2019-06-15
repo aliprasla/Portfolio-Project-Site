0204afec596f9292c5fe74e64b2e283c63dda02b
Ali Prasla <ali@wellsuited.com>
2019-06-15 16:53:08 -0500

    Padding and completed navigation

--- projects/urls.py
+++ projects/urls.py
@@ -6,8 +6,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='projects-home'),
-    path('about',views.about,name='projects-another')
+    path('',views.home,name='home'),
+    path('about', views.about, name='about')
 ]
 
 
