57cd08f462139c132fa6e663b360c50ff07e40e4
Ali Prasla <aprasla0922@gmail.com>
2020-04-28 16:37:53 -0500

    Ali/mnist flask api (#11)
    * Updated project model with long and short description
    
    * Working on base templating and URL routing
    
    Co-authored-by: Ali Prasla <ali@wellsuited.com>

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,9 @@ from django.forms import ModelForm
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=750)
-    description = models.TextField()
+    short_description = models.TextField(default="")
+    long_description = models.TextField(default="")
     last_modified_on = models.DateTimeField(auto_now=True)
+    url = models.URLField(default="")
 
 
--- projects/urls.py
+++ projects/urls.py
@@ -2,12 +2,15 @@
 Url routing for projects app
 """
 
-from django.urls import path
+from django.urls import path,re_path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('index_js',views.index_js,name='index_js'),
+    re_path(r'^projects/(.*)',views.generic_project_view,name='project_view')
 ]
 
 
